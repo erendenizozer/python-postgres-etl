82164498f9af07fd3649d582930327a1146c8b23
Deniz Eren Ozer <denizerenozer@icloud.com>
2026-08-14 01:25:39 +0300

    Added transformation and validation in transform.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from extract import extract_data
+from transform import transform_posts
 from load import create_table, insert_posts
 
 posts = extract_data()
 
+transformed_posts = transform_posts(posts)
+
 create_table()
 
-insert_posts(posts)
+insert_posts(transformed_posts)
 
 print("ETL completed successfully!")
